64cb60307a6785ca68fff9e60a13cc55adeaed70
Aliya Gamez <aliya.e.gamez@gmail.com>
2023-11-29 12:01:26 -0500

    JS button

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+//Makes Featured Recipe Button A Link to Recipe
+let featuredRecipeBtn = document.getElementById("featured-recipe-btn");
+featuredRecipeBtn.addEventListener("click", function() {
+    window.location.href = "creamy_key_lime_pie.html";
+});
