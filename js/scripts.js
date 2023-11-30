20b224a0dbcf46cc7d721e91ea98288180d4c02d
Aliya Gamez <aliya.e.gamez@gmail.com>
2023-11-29 21:03:04 -0500

    Code to get recipe and put on front end

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,8 @@ let featuredRecipeBtn = document.getElementById("featured-recipe-btn");
 featuredRecipeBtn.addEventListener("click", function() {
     window.location.href = "recipe_id/1/creamy_key_lime_pie.html";
 });
+
+//Fetch Recipes From Recipes Table
+document.addEventListener("DOMContentLoaded", function () {
+
+});
