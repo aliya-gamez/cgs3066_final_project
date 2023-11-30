dd7f41b32df9dcb2729c76b3ce5289ac807d4202
Aliya Gamez <aliya.e.gamez@gmail.com>
2023-11-29 21:06:50 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,12 @@ featuredRecipeBtn.addEventListener("click", function() {
     window.location.href = "recipe_id/1/creamy_key_lime_pie.html";
 });
 
-//Fetch Recipes From Recipes Table
+//Creates database and populates it with some recipes
+document.addEventListener("DOMContentLoaded", function () {
+
+});
+
+//Get recipes table data and input on front-end
 document.addEventListener("DOMContentLoaded", function () {
 
 });
