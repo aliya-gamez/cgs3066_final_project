f2f89d19871f1e07e1171f86659be15327a19c3e
Aliya Gamez <aliya.e.gamez@gmail.com>
2023-12-12 11:10:45 -0500

    added styling to delete btn

--- php/add_recipe.php
+++ php/add_recipe.php
@@ -88,7 +88,7 @@ try {
                     <!-- Add more details as needed -->
                 </div>
                 </section>
-                <section>
+                <section class='view-recipe'>
                     <p>
                         <form class='delete-form' method='POST' action='../php/delete_recipe.php'>
                             <input type='hidden' name='recipe_id' value='$recipeId'>
